ea32327624f285f79937bbd20d2a41be6c6de0ae
Laura Szarzyńska <235987@edu.p.lodz.pl>
2022-05-16 08:15:59 +0200

    zad13 Przeniesienie Node do IntLinkedList oraz hermetyzacja tejze do private

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -48,4 +48,27 @@ public class IntLinkedList {
 	public int getI() {
 		return i;
 	}
+
+	private static class Node {
+
+		private int value;
+		private Node prev;
+		protected Node next;
+
+		public Node(int i) {
+			this.value=i;
+		}
+
+		public int getValue() {
+			return value;
+		}
+
+		public Node getPrev() {
+			return prev;
+		}
+
+		public void setPrev(Node prev) {
+			this.prev = prev;
+		}
+	}
 }
